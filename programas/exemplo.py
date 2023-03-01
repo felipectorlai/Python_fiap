35b4af9fc018b5de1145f784104f3504e6cca83b
Logon Aluno <logonrmlocal@fiap.com.br>
2023-03-01 20:50:41 -0300

    Initial commit

---
+++ programas/exemplo.py
@@ -0,0 +1,28 @@
+print("Meu primeiro programa")
+
+#Exemplo de tipo numerico inteiro (int)
+numero = 6
+
+print(type(numero))
+
+print("Valor do numero eh: ", numero)
+
+#Exemplo de tipo numerico real (float)
+valor_salario = 5400.30
+
+print(type(valor_salario))
+
+print("Valor do salario: ", valor_salario)
+
+#Exemplo de entradas de dados
+mensagem = input("Digite uma mensagem de texto: ")
+
+print(mensagem)
+
+valor = input("Digite um valor: ")
+valor = int(valor)
+
+print(type(valor))
+
+a = int(input("Digite um valor de a: "))
+
---
+++ programas/soma_numero.py
@@ -0,0 +1,11 @@
+#Programa para somar 2 numeros inteiros
+
+#ENTRADA DE DADOS
+num1 = int(input("Digite o primeiro numero: "))
+num2 = int(input("Digite o segundo numero: "))
+
+#PROCESSAMENTO
+res = num1 + num2
+
+#SAIDA
+print("A soma dos numeros eh: ", res)
